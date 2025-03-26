11f7410bac6b52a2608146cd96db7a401c063482
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2025-03-26 08:15:46 +0700

    add test coverage

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
   test: {
     globals: true,   // This enables the global test functions like describe, it, expect
     environment: 'jsdom',  // If you're testing a DOM-based application, set it to 'jsdom'
+    coverage: {
+      reporter: ['text', 'lcov', 'json'],  // You can choose the reporters you want (text, lcov, json, etc.)
+      all: true, // Collect coverage for all files, not just the ones tested
+      include: ['src/**/*.js', 'src/**/*.vue'],  // Path to the files you want to include in the coverage
+      exclude: ['src/**/*.test.js', 'src/**/*.test.vue'],  // Exclude test files from coverage
+    },
   },
   resolve: {
     alias: {
