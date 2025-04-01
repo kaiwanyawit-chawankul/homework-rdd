4ba0c84f64919efe6149e78a4ed1414ee905e64a
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2025-04-01 10:08:47 +0700

    Revert "try fix ci"
    This reverts commit f8996bd534d7d670d53dec5e2c97af1afa9ea0fc.

--- prepare.cjs
+++
@@ -1,7 +0,0 @@
-// prepare.js
-if (!(process.env.NODE_ENV === 'production' || process.env.CI === 'true')) {
-    const { execSync } = require('child_process');
-    execSync('npx husky install', { stdio: 'inherit' });
-  } else {
-    console.log('Skipping husky installation: Production or CI environment detected.');
-  }
