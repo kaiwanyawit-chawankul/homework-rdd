484648b01571099b2f554d9545c3a00170ef2905
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2025-04-04 16:51:10 +0700

    fix sonar

--- packages/e2e/cypress/support/commands.js
+++ packages/e2e/cypress/support/commands.js
@@ -1,8 +1,19 @@
 import "cypress-file-upload";
 
 Cypress.Commands.add("uploadFile", (fileName) => {
-  // Path to your fixture file
-  const filePath = `${fileName}`;
+  // Ensure fileName is a string
+  if (typeof fileName !== "string") {
+    throw new Error("The fileName parameter must be a string");
+  }
 
+  // Path to your fixture file
+  const filePath = fileName.trim(); // Optionally, trim any extra spaces
+
+  // Check if the file exists or handle the case where it's not available
+  if (!filePath) {
+    throw new Error("File name cannot be empty");
+  }
+
+  // Upload the file using Cypress file upload
   cy.get('input[type="file"]').attachFile(filePath);
 });
--- packages/e2e/cypress/support/e2e.js
+++ packages/e2e/cypress/support/e2e.js
@@ -1,8 +1,11 @@
 // cypress/support/e2e.js
 import "./commands"; // Assuming you have custom commands in 'commands.js'
 
-// // Any global setup or configuration
-// Cypress.on('uncaught:exception', (err, runnable) => {
-//   // Prevent Cypress from failing the test on uncaught exceptions
-//   return false;
-// });
+// Any global setup or configuration
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // Prevent Cypress from failing the test on uncaught exceptions
+  console.error("Uncaught exception:", err);
+  console.info("Test that caused the exception:", runnable);
+
+  return false;
+});
