a094a301ae2d77ffc3e385b5fdaa85d0451d66b2
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2025-04-04 22:39:03 +0700

    fix style

--- packages/app/src/components/ATSPreview.vue
+++ packages/app/src/components/ATSPreview.vue
@@ -1,75 +1,85 @@
 <template>
-    <div class="resume">
-      <div class="contact-info">
-        <h1>{{ resume.contact.name }}</h1>
-        <p>{{ resume.contact.address }}</p>
-        <p>Email: {{ resume.contact.email }} | Phone: {{ resume.contact.mobile }}</p>
-      </div>
-
-      <section v-if="resume.description" class="description">
-        <h2>Objective:</h2>
-        <p>{{ resume.description }}</p>
-      </section>
-
-      <section v-if="resume.skills.length" class="skills">
-        <h2>{{ resume.skills[0].title }}</h2>
-        <ul>
-          <li v-for="(skill, index) in resume.skills[0].items" :key="index">
-            {{ skill.name }}
-          </li>
-        </ul>
-      </section>
-
-      <section v-if="resume.skills[1]" class="tech-stacks">
-        <h2>{{ resume.skills[1].title }}</h2>
-        <ul>
-          <li v-for="(tech, index) in resume.skills[1].items" :key="index">
-            {{ tech.name }}
-          </li>
-        </ul>
-      </section>
-
-      <section v-if="resume.experiences.length" class="experiences">
-        <h2>Work Experience:</h2>
-
-          <section v-for="(experience, index) in resume.experiences" :key="index">
-            <h3>{{ experience.title }} | {{ experience.company }} | {{ formatMonthYear(experience.startDate) }} - {{ formatMonthYear(experience.endDate) }}</h3>
-            <ul>
-              <li v-for="(task, tIndex) in experience.tasks" :key="tIndex">
-                {{ task }}
-              </li>
-            </ul>
-          </section>
-
-      </section>
-
-      <section v-if="resume.educations.length" class="education">
-        <h2>Education:</h2>
-        <ul>
-          <li v-for="(education, index) in resume.educations" :key="index">
-            <p>{{ education.degree }} | {{ education.school }} | Graduated {{ formatMonthYear(education.endDate) }}</p>
-          </li>
-        </ul>
-      </section>
-
-      <section v-if="resume.others.length" class="others">
-        <div v-for="(other, index) in resume.others" :key="index">
-          <h2>{{ other.title }}:</h2>
-          <ul>
-            <li v-for="(item, itemIndex) in other.items" :key="itemIndex">{{ item }}</li>
-          </ul>
-        </div>
-      </section>
+  <div class="resume">
+    <div class="contact-info">
+      <h1>{{ resume.contact.name }}</h1>
+      <p>{{ resume.contact.address }}</p>
+      <p>
+        Email: {{ resume.contact.email }} | Phone: {{ resume.contact.mobile }}
+      </p>
     </div>
-  </template>
 
-  <script>
-  export default {
-    name: "ATSResume",
-    props: {
-        resume: Object,
-    },
-    methods: {
+    <section v-if="resume.description" class="description">
+      <h2>Objective:</h2>
+      <p>{{ resume.description }}</p>
+    </section>
+
+    <section v-if="resume.skills.length" class="skills">
+      <h2>{{ resume.skills[0].title }}</h2>
+      <ul>
+        <li v-for="(skill, index) in resume.skills[0].items" :key="index">
+          {{ skill.name }}
+        </li>
+      </ul>
+    </section>
+
+    <section v-if="resume.skills[1]" class="tech-stacks">
+      <h2>{{ resume.skills[1].title }}</h2>
+      <ul>
+        <li v-for="(tech, index) in resume.skills[1].items" :key="index">
+          {{ tech.name }}
+        </li>
+      </ul>
+    </section>
+
+    <section v-if="resume.experiences.length" class="experiences">
+      <h2>Work Experience:</h2>
+
+      <section v-for="(experience, index) in resume.experiences" :key="index">
+        <h3>
+          {{ experience.title }} | {{ experience.company }} |
+          {{ formatMonthYear(experience.startDate) }} -
+          {{ formatMonthYear(experience.endDate) }}
+        </h3>
+        <ul>
+          <li v-for="(task, tIndex) in experience.tasks" :key="tIndex">
+            {{ task }}
+          </li>
+        </ul>
+      </section>
+    </section>
+
+    <section v-if="resume.educations.length" class="education">
+      <h2>Education:</h2>
+      <ul>
+        <li v-for="(education, index) in resume.educations" :key="index">
+          <p>
+            {{ education.degree }} | {{ education.school }} | Graduated
+            {{ formatMonthYear(education.endDate) }}
+          </p>
+        </li>
+      </ul>
+    </section>
+
+    <section v-if="resume.others.length" class="others">
+      <div v-for="(other, index) in resume.others" :key="index">
+        <h2>{{ other.title }}:</h2>
+        <ul>
+          <li v-for="(item, itemIndex) in other.items" :key="itemIndex">
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ATSResume",
+  props: {
+    resume: Object,
+  },
+  methods: {
     formatYear(dateString) {
       const date = new Date(dateString);
       return date.getFullYear(); // Extracts the year (2005)
@@ -86,8 +96,8 @@
       return `${month} ${year}`;
     },
   },
-  };
-  </script>
+};
+</script>
 
 <style scoped>
 .resume {
@@ -132,12 +142,14 @@ li {
   margin-bottom: 20px;
 }
 
-.skills ul, .tech-stacks ul {
+.skills ul,
+.tech-stacks ul {
   display: flex;
   flex-wrap: wrap;
 }
 
-.skills li, .tech-stacks li {
+.skills li,
+.tech-stacks li {
   margin-right: 15px;
 }
 
